634d575b9150c48b8ad00ee10b9825aef22f23a6
Raj Markana <rajmarkana1324@gmail.com>
2024-03-18 23:11:59 +0530

    Fix:Image Issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="px-8 flex flex-col justify-center items-center">
                 <div data-aos="fade-down" data-aos-duration="1000">
 
-                    <h1  className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary mb-8"><span className='px-1 bg-primary text-white leading-relaxed mr-2'>Unleash </span> Yourr Potential With  <br />Limitless Opportunities</h1>
+                    <h1  className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary mb-8"><span className='px-1 bg-primary text-white leading-relaxed mr-2'>Unleash </span> Your Potential With  <br />Limitless Opportunities</h1>
                     <p  className="text-lg text-secondary mb-8">
                         Discover a world of opportunities with internships,projects, and a gateway to your professional future.
                     </p>
